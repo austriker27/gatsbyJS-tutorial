2cbd40d74bfb94db4ecb3c0a2c03995e86f7c8fb
David Lindahl <davidalindahl@gmail.com>
2018-02-27 14:03:09 -0800

    finished tutorial by adding in links and blog post templates

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import g from "glamorous";
+import Link from "gatsby-link";
+
 
 import { rhythm } from "../utils/typography";
 
@@ -13,11 +15,16 @@ export default ( {data} ) => {
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <g.H3 marginBottom={rhythm(1 / 4)}>
-            {node.frontmatter.title}{" "}
-            <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
-          </g.H3>
-          <p>{node.excerpt}</p>
+          <Link
+            to={node.fields.slug}
+            css={{ textDecoration: `none`, color: `inherit` }}
+            >
+              <g.H3 marginBottom={rhythm(1 / 4)}>
+                {node.frontmatter.title}{" "}
+                <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+              </g.H3>
+              <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -34,6 +41,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
