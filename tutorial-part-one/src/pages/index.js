a80ee490e75bf83662effd6db35c21cc8f5a949d
David Lindahl <davidalindahl@gmail.com>
2018-02-27 11:34:26 -0800

    made counter

--- tutorial-part-one/src/pages/index.js
+++ tutorial-part-one/src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div style={{ color: `blue` }}>
+  <h1>Hello gatsby!</h1>
+  <p>what a world.</p>
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <br/>
+  <div>
+    <Link to="/page-2/">Link</Link>
+  </div>
+  <div>
+    <Link to="/counter/">Counter</Link>
+  </div>
+</div>
