66e713701a1a661777457eef0de06a78d2ea9c90
David Lindahl <davidalindahl@gmail.com>
2018-02-27 13:30:58 -0800

    adding layout and theme and files

---
+++ tutorial-part-four/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1> About pandas</h1>
+    <p>
+      they like eating food
+    </p>
+  </div>
+);
--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>Amazing Pandas eating Things</h1>
+    <div>
+      <img src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+      alt="pandas eating"
+      />
+    </div>
+  </div>
+)
