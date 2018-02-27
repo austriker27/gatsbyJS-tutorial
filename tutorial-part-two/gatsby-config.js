43b52165fe5ff89fe8fab9f11907020d49fed4c5
David Lindahl <davidalindahl@gmail.com>
2018-02-27 11:47:46 -0800

    bootstrap theme

--- tutorial-part-two/gatsby-config.js
+++ tutorial-part-two/gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typography`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
 };
--- tutorial-part-two/src/utils/typography.js
+++ tutorial-part-two/src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from "typography";
+import bootstrapTheme from "typography-theme-bootstrap";
 
-const typography = new Typography({ baseFontSize: "18px" });
+const typography = new Typography(bootstrapTheme);
 
 export default typography;
